e48887ed2a8be5302336707492ed319b2f888906
sonal.chaplot <sonal.chaplot@dieboldnixdorf.com>
2021-05-18 21:03:40 +0530

    Updating product styles

--- client/src/components/Products/product.jsx
+++ client/src/components/Products/product.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
-import {
-  Container,
-  ProductImage,
-  ProductName,
-  ProductPriceDetails,
-  Savings,
-  ActualPrice,
-  BoughtPrice,
-  ProductRating,
-  ProductCategory,
-} from './products.style';
-import useStyles from './products.style';
 
+import './products.css';
 import Rating from '@material-ui/lab/Rating';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { useDispatch } from 'react-redux';
@@ -19,7 +8,6 @@ import { deleteProduct } from '../../redux/products/products.actions';
 import { getImage } from '../../utils/imageDecoding';
 
 export const Product = ({ product }) => {
-  const classes = useStyles();
   const dispatch = useDispatch();
 
   const removeProduct = (product) => {
@@ -29,23 +17,35 @@ export const Product = ({ product }) => {
     return product.actualPrice - product.boughtPrice;
   };
   return (
-    <Container>
-      <ProductImage src={getImage(product?.image?.data?.data)}></ProductImage>
-      <ProductName>{product.name}</ProductName>
-      <ProductCategory>Category: {product.category?.title}</ProductCategory>
-      <ProductPriceDetails>
-        <ActualPrice>Actual Price : Rs {product.actualPrice}</ActualPrice>
-        <BoughtPrice>Bought At: Rs {product.boughtPrice}</BoughtPrice>
-        <Savings>Savings: Rs {calculateDiscount()}</Savings>
-      </ProductPriceDetails>
-      <ProductRating>
+    <div className='product-container'>
+      <img
+        className='product-container__icon'
+        src={getImage(product?.image?.data?.data)}
+        alt={product.name}
+      ></img>
+      <p className='product-container__name'>{product.name}</p>
+      <p className='product-container__category'>
+        Category: {product.category?.title}
+      </p>
+      <div className='product-container__price-container'>
+        <p className='product-container__actual-price'>
+          Actual Price : Rs {product.actualPrice}
+        </p>
+        <p className='product-container__bought-price'>
+          Bought At: Rs {product.boughtPrice}
+        </p>
+        <p className='product-container__savings'>
+          Savings: Rs {calculateDiscount()}
+        </p>
+      </div>
+      <div>
         <Rating name='read-only' value={product.rating} readOnly />
         <DeleteIcon
           color='error'
-          className={classes.delete}
+          className='product-container__delete'
           onClick={() => removeProduct(product)}
         />
-      </ProductRating>
-    </Container>
+      </div>
+    </div>
   );
 };
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
-import { ProductList } from '../../components/Products/product.list';
+import { Button, Grid, Typography } from '@material-ui/core';
+import Add from '@material-ui/icons/Add';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchProducts } from '../../redux/products/products.actions';
+import { Product } from '../../components/Products/product';
 
+import BounceLoader from 'react-spinners/BounceLoader';
+import { css } from '@emotion/core';
+
+import './Product.style.css';
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: green;
+`;
 export const Products = () => {
+  const { isLoading, items } = useSelector((state) => ({
+    isLoading: state.products.isLoading,
+    items: state.products.items,
+  }));
+
+  let [color, setColor] = useState('#96C5BD');
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
   return (
-    <div>
-      <ProductList />
-    </div>
+    <>
+      <div className='product-container-list'>
+        <Link to='/addProduct'>
+          <Button
+            startIcon={<Add />}
+            variant='contained'
+            className='product-container__Add'
+          >
+            Add Product
+          </Button>
+        </Link>
+      </div>
+      <BounceLoader
+        color={color}
+        loading={isLoading}
+        css={override}
+        size={150}
+      />
+      <Grid container className='product-container__root' spacing={2}>
+        {items.length <= 0 && (
+          <Typography variant='h6'>No Products to display.</Typography>
+        )}
+        {items.length > 0 &&
+          items.map((product) => (
+            <Product product={product} key={product.id} />
+          ))}
+      </Grid>
+    </>
   );
 };
